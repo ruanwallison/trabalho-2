047e48e4106001ba8ff4147bf815af85a9cb2ad7
Fgante <fegante@hotmail.com>
2015-06-08 11:00:13 -0300

    Create trab2.c

---
+++ trab2.c
@@ -0,0 +1,23 @@
+typedef struct veiculo {
+	char numChassi[30],modelo[15],cor[10],placa[15],opcionais[30][6];
+	int ano;
+	float diaria;
+} *veiculos;
+
+typedef struct {
+	int dia,mes,ano
+}data;
+
+
+typedef struct cliente{
+	char cpf[20],nome[30],sexo;
+	data datNascimento; 
+	int chn;
+}*clientes;
+
+typedef struct locacao{
+	veiculo veiLocad;
+	clientes clienLocad;
+	int locad;
+}*locacaos;
+
